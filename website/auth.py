c28d9c7637e24b86d973fd1ecf49d8f4ce2edaf4
Nohemi Morales <nohemi@umail.ucsb.edu>
2023-01-12 15:44:49 -0800

    implemented sign-up route

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -17,23 +20,27 @@ def sign_up():
     if request.method == 'POST':
         # Gather user data
         email = request.form.get('email')
-        name = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         # Validate user data
         if not email:
             flash('Please enter a valid email.', category='error')
-        elif not name:
+        elif not first_name:
             flash('Please enter a valid name.', category='error')
         elif password1 != password2:
             flash('Passwords do not match.', category='error') 
         elif len(password1) < 7:
             flash('Password must be at least 7 characters', category='error')
         else:
-            # Create an account and push to databases
-            
+            # Create an account 
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            # Push to database
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     # else (request method must be get) we direct user to sign up
     return render_template("sign_up.html")
