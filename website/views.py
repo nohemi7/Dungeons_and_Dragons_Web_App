1c3b509c9f8fa2d94a939951e99a0781d365f742
Nohemi Morales <nohemi@umail.ucsb.edu>
2023-01-12 18:29:09 -0800

    Optimized Design for template using jinja

--- website/views.py
+++ website/views.py
@@ -4,7 +4,58 @@ from flask_login import login_required, current_user
 #Set up blueprint for our flask app
 views = Blueprint('views', __name__)
 
-# Routes
+# Global Variables
+char_races = [
+    "Dragonborn",
+    "Dwarf",
+    "Elf",
+    "Gnome",
+    "Half-Elf",
+    "Halfling",
+    "Half-Orc",
+    "Human",
+    "Tiefling"
+]
+char_classes = [
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorcerer",
+    "Warlock",
+    "Wizard"
+]
+char_backgrounds = [
+    "Acolyte",
+    "Charlatan",
+    "Criminal",
+    "Entertainer",
+    "Folk Hero",
+    "Guild Artisan",
+    "Hermit",
+    "Noble",
+    "Outlander",
+    "Sage",
+    "Sailor",
+    "Soldier",
+    "Urchin"
+]
+char_alignments = [
+    "Lawful Good",
+    "Lawful Neutral",
+    "Lawful Evil",
+    "Neutral Good",
+    "True Neutral",
+    "Neutral Evil",
+    "Chaotic Good",
+    "Chaotic Neutral",
+    "Chaotic Evil"
+]
 
 # Homepage route
 @views.route('/')
@@ -17,4 +68,5 @@ def home():
 @views.route('/char-creator', methods=['GET', 'POST'])
 @login_required
 def char_creator():
-    return render_template("char_creator.html", user=current_user)
+    return render_template("char_creator.html", user=current_user, char_races=char_races, 
+        char_classes=char_classes, char_backgrounds=char_backgrounds, char_alignments=char_alignments)
